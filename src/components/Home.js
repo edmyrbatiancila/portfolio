3d71067ae2b6bcb438819c188576f47172fd5901
edBatiancila <127021800+edmyrbatiancila@users.noreply.github.com>
2024-04-08 01:35:46 +0800

    Add necessary files/folder
    Please note that the 'node_modules' folder was intentionally removed because it contained large files.

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeContents from './HomeContents';
+import ProjectLists from './ProjectLists'
+
+function Home() {
+    return(
+        <div>
+            <HomeContents/>
+            <ProjectLists />
+        </div>
+
+    );
+}
+
+export default Home;
